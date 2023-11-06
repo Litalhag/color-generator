2d26c9e4cefdc11d36ad416a4aa933bc3bfa167d
Litalhag <litalhag@gmail.com>
2023-11-06 18:05:03 +0200

    singleColor and ToastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react'
 import ColorList from './components/ColorList'
 import Form from './components/Form'
+import Values from 'values.js'
+import { ToastContainer, toast } from 'react-toastify'
 
 const App = () => {
+  const [colors, setColors] = useState(new Values('#f15025').all(10))
+  // toast.success('awesome')
+  //
+
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(10)
+      setColors(newColors)
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   return (
     <main>
-      <Form />
-      <ColorList />
+      <Form addColor={addColor} />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   )
 }
